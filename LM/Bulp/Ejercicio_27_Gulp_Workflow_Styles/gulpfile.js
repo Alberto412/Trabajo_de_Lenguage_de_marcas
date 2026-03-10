e1d0ccdb0ea293223000070787aff38389a7db73
Alberto412 <anavmol2011@g.educaand.es>
2026-03-10 10:57:04 +0100

    cambios en node

--- LM/Bulp/Ejercicio_27_Gulp_Workflow_Styles/gulpfile.js
+++ LM/Bulp/Ejercicio_27_Gulp_Workflow_Styles/gulpfile.js
@@ -2,3 +2,10 @@
 // 1. Importa gulp y gulp-sass
 // 2. Define una tarea 'css' para compilar
 // 3. Exporta la tarea default con watch
+const { src, dest, watch } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+function css (){
+    return SRC ("src/scss/*.scss")
+    .pipe(sass('').on("error",sass.logError))
+    
+}
