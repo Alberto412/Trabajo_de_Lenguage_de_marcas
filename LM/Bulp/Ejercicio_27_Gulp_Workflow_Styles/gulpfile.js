85df830d87d82b608fc9b19fc38ad9a94b2393d6
Alberto412 <anavmol2011@g.educaand.es>
2026-03-10 12:05:12 +0100

    e 27

--- LM/Bulp/Ejercicio_27_Gulp_Workflow_Styles/gulpfile.js
+++ LM/Bulp/Ejercicio_27_Gulp_Workflow_Styles/gulpfile.js
@@ -5,7 +5,11 @@
 const { src, dest, watch } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 function css (){
-    return SRC ("src/scss/*.scss")
-    .pipe(sass('').on("error",sass.logError))
-    
+    return src ("src/scss/*.scss")
+    .pipe(sass().on("error",sass.logError))
+    .pipe(dest('dist/css'));
 }
+exports.css = css;
+exports.default = function(){
+    watch("src/scss/*.scss",css);
+};
